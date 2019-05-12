7de72a9f658d107b45eb2036fcb427eeb5e0a1a5
Ashmi Chheda <ashmichheda@gmail.com>
2019-05-12 20:16:51 +0530

    added puzzle 2 solution

---
+++ 2-expressive-puzzlers/puzzle-2/Change_Solution.java
@@ -0,0 +1,25 @@
+/*
+
+Consider the following word problem: Tom goes to the auto parts store to buy a spark plug that costs $1.10, but all he
+has in his wallet are two-dollar bills. How much change should he get if he pays
+for the spark plug with a two-dollar bill?
+
+Change.solution is the program that attempts to solve the problem. What does it print?
+
+If i were to manually subtract the 2 numbers, i would expect the output to be 0.9
+But the program prints 0.8999999999999999.
+
+Actually the problem is that 1.1 can't be represented exactly as double, so it's represented by its closest double value.
+Hence one should avoid using float or double in monetary calculations & INSTEAD use int or long or BigDecimal
+
+
+*/
+
+
+
+import java.Math.BigDecimal;
+public class Change {
+    public static void main(String args[]) {
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.10")));
+    }
+}
