60fc1332cf5e96c8e2f7875b394212b682cca244
Ashmi Chheda <ashmichheda@gmail.com>
2019-05-20 00:03:31 +0530

    update puzzle 4 solution

--- 2-expressive-puzzlers/puzzle-4/Elementary_Solution.java
+++ 2-expressive-puzzlers/puzzle-4/Elementary_Solution.java
@@ -1,7 +1,7 @@
 /*
 
 By merely looking at the program, one might feel that we are adding two "INT" operands which should fetch 66666 as the answer!
-But the answer comes out to be 17777. This is because, one of the operand is INT and the other is LONG!
+But, the answer comes out to be 17777. This is because, one of the operand is 'INT' and the other is 'LONG'.
 PRACTICE is to always use Capital letter 'L' while using long literals.
 
 
@@ -12,7 +12,7 @@ PRACTICE is to always use Capital letter 'L' while using long literals.
 public class Elementary {
     public static void main(String[] args) {
 		
-				
+		// change small el 'l' literal to capital 'L'		
         System.out.println(12345 + 5432L);
     }
 }
